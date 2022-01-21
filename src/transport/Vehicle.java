329c9f3aba8e223823f37978f07d7fbde4fdc522
panda404 <yakhyoev.temur@mail.ru>
2022-01-21 23:55:00 +0400

    Assignments on Inheritance (super constructor added)

--- src/transport/Vehicle.java
+++ src/transport/Vehicle.java
@@ -13,6 +13,12 @@ public class Vehicle extends Transport {
         this.numOfWheels = numOfWheels;
     }
 
+    public Vehicle(String company, String model, String color, int maxCapacity,
+                   double weight, int maxSpeed, int price, int productionYear, int numOfWheels) {
+        super(company, model, color, maxCapacity, weight, maxSpeed, price, productionYear);
+        this.numOfWheels = numOfWheels;
+    }
+
     //getters and setters
     public int getNumOfWheels() {
         return numOfWheels;
